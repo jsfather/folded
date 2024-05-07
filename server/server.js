8d25b94d1f6c472a1c046531110b16764a29286d
Keyvan Matinrad <keyvanmatinrad@outlook.com>
2024-05-07 14:16:35 +0330

    first commit

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+    console.log(req.path , req.method);
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.json({msg: "Hello World!"});
+})
+
+app.listen(port , ()=>{
+    console.log(`Server started at http://localhost:${port}`);
+})
