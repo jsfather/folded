f5507fc4ee250def6f3bd96e8c97db12113952ca
Keyvan Matinrad <keyvanmatinrad@outlook.com>
2024-05-07 20:27:14 +0330

    added material ui

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,15 @@
+import Button  from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import {Fingerprint} from "@mui/icons-material";
+
 const Home = () => {
     return (
         <div className="home">
             <h2>Home</h2>
+            <Button variant="contained">Hello world</Button>
+            <IconButton color="primary">
+                <Fingerprint />
+            </IconButton>
         </div>
     )
 }
