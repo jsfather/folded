ca9b5eadafd7b7358846a0b7c28115a78a727810
jsfather <keyvanmatinrad@outlook.com>
2024-11-14 17:37:42 +0330

    Footer replaced by Navbar

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from './ui/Button';
 export { default as Header } from './layout/Header';
-export { default as Footer } from './layout/Footer';
+export { default as Navbar } from './layout/Navbar';
