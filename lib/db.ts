7cb20992826bd77db5b4bb37b8427e3db4d0e67f
jsfather <keyvanmatinrad@outlook.com>
2024-11-26 16:25:41 +0330

    database connection info is now in env file

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose';
 
+const mongo_uri = process.env.MONGO_URI || '';
+
 const dbConnect = () => {
-  mongoose
-    .connect(
-      'mongodb+srv://keyvanmatinrad:vioDUQXde8XI4KQ3@cluster0.znm85.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    )
-    .then(() => {
-      console.log('Connected to DB');
-    });
+  mongoose.connect(mongo_uri).then(() => {
+    console.log('Connected to DB');
+  });
 };
 export default dbConnect;
