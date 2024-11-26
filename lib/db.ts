a61bd72cbab7b17822a10d134dd8129d73282824
jsfather <keyvanmatinrad@outlook.com>
2024-11-26 16:54:02 +0330

    isConnected state added

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,25 @@
 import mongoose from 'mongoose';
 
-const mongo_uri = process.env.MONGO_URI || '';
+const MONGO_URI = process.env.MONGO_URI || '';
 
-const dbConnect = () => {
-  mongoose.connect(mongo_uri).then(() => {
-    console.log('Connected to DB');
-  });
-};
-export default dbConnect;
+let isConnected = false;
+
+export async function connectDb() {
+  if (isConnected) {
+    console.log('Database is already connected');
+    return;
+  }
+
+  if (!MONGO_URI) {
+    throw new Error('Please define the MONGO_URI environment variable');
+  }
+
+  try {
+    const db = await mongoose.connect(MONGO_URI);
+    isConnected = db.connection.readyState === 1;
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed', error);
+    throw error;
+  }
+}
