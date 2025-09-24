26af14368173cc9b028077317ef8c6c8a8ce094e
jsfather <keyvanmatinrad@outlook.com>
2025-09-24 14:55:14 +0330

    refactor: remove unnecessary line break in main section of page component

--- app/page.tsx
+++ app/page.tsx
@@ -29,8 +29,7 @@ export default async function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-          </main>
+          <main className="flex-1 flex flex-col gap-6 px-4"></main>
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
